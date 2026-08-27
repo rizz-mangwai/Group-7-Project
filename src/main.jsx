f0fa67cadf8be588cbdb9e5313e8d3278d7ec6a1
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-08-27 22:44:09 +0300

    new

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>HALOO</h1>
+  </StrictMode>,
+)
